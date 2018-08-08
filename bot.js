cf65136710368a7a4c3b50a2828d9928983afd49
fferidd51 <42213592+fferidd51@users.noreply.github.com>
2018-08-08 21:34:24 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- client.on('ready', () => {
+
+client.on('ready', () => {
     console.log('I am ready!');
 });
- client.on('message', message => {
+
+client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
- // THIS  MUST  BE  THIS  WAY
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
